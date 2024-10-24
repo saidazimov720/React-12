6800e246f0be3995539c91af563c277342744fec
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-10-24 15:23:14 +0500

    finished only the key left

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MovieList from './Components/MovieList';
+import MovieDetails from './Components/MovieDetails';
+import Favorites from './Components/Favourites';
+import Watchlist from './Components/WatchList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
 
 export default App;
